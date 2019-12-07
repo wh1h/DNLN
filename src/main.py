bded5aa83a730070d5050a1838c1a500d70caea6
wh1h <39109242+wh1h@users.noreply.github.com>
2019-12-07 16:16:23 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,12 @@ import os
 torch.manual_seed(args.seed)
 checkpoint = utility.checkpoint(args)
 
-
+def print_network(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    print('Total number of parameters: %.2f M'%(num_params/1000000))
+    
 def main():
     global model
     if args.data_test == ['video']:
@@ -24,6 +29,11 @@ def main():
         if checkpoint.ok:
             loader = data.Data(args)
             _model = model.Model(args, checkpoint)
+            
+            print('----------------------------------------------')
+            print_network(_model)
+            print('----------------------------------------------')
+            
             _loss = loss.Loss(args, checkpoint) if not args.test_only else None
             t = Trainer(args, loader, _model, _loss, checkpoint)
             while not t.terminate():
